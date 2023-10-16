782116a0ab92e1c57e3160ecd5eff1325b90d3e5
pramod-dev <pramod.munnelli@gmail.com>
2023-10-16 13:35:23 -0400

    print only actual log entries without logging errors

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,45 @@
-"use strict";
+module.exports = async (logSources, printer) => {
+  try {
+    // Define a function to fetch the next log entry from a source
+    const getNextLogEntry = async (logSource) => {
+      const logEntry = await logSource.popAsync();
+      if (logEntry) {
+        return { source: logSource, entry: logEntry };
+      }
+      return null;
+    };
 
-// Print all entries, across all of the *async* sources, in chronological order.
+    // Create a priority queue to keep track of the next log entry from each source
+    const PriorityQueue = require("fastpriorityqueue");
+    const queue = new PriorityQueue((a, b) => a.entry.date - b.entry.date);
 
-module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
-  });
+    // Initialize the queue with the first log entry from each source
+    for (const logSource of logSources) {
+      const logEntry = await getNextLogEntry(logSource);
+      if (logEntry) {
+        queue.add(logEntry);
+      }
+    }
+
+    // Process and print log entries in chronological order
+    while (!queue.isEmpty()) {
+      const { source, entry } = queue.poll();
+      if (entry.date >= source.last.date) {
+        // Print the log entry only if it satisfies the condition
+        printer.print(entry);
+      }
+
+      // Fetch the next log entry from the source and add it to the queue
+      const nextLogEntry = await getNextLogEntry(source);
+      if (nextLogEntry) {
+        queue.add(nextLogEntry);
+      }
+    }
+
+    // Print statistics
+    printer.done();
+  } catch (error) {
+    console.error("An error occurred:", error);
+    // Handle the error as needed
+  }
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,45 @@
-"use strict";
-
-// Print all entries, across all of the sources, in chronological order.
-
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+  try {
+    // Define a function to fetch the next log entry from a source
+    const getNextLogEntry = (logSource) => {
+      const logEntry = logSource.pop();
+      if (logEntry) {
+        return { source: logSource, entry: logEntry };
+      }
+      return null;
+    };
+
+    // Create a priority queue to keep track of the next log entry from each source
+    const PriorityQueue = require("fastpriorityqueue");
+    const queue = new PriorityQueue((a, b) => a.entry.date - b.entry.date);
+
+    // Initialize the queue with the first log entry from each source
+    for (const logSource of logSources) {
+      const logEntry = getNextLogEntry(logSource);
+      if (logEntry) {
+        queue.add(logEntry);
+      }
+    }
+
+    // Process and print log entries in chronological order
+    while (!queue.isEmpty()) {
+      const { source, entry } = queue.poll();
+      if (entry.date >= source.last.date) {
+        // Print the log entry only if it satisfies the condition
+        printer.print(entry);
+      }
+
+      // Fetch the next log entry from the source and add it to the queue
+      const nextLogEntry = getNextLogEntry(source);
+      if (nextLogEntry) {
+        queue.add(nextLogEntry);
+      }
+    }
+
+    // Print statistics
+    printer.done();
+  } catch (error) {
+    console.error("An error occurred:", error);
+    // Handle the error as needed
+  }
 };
